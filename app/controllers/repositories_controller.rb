e17efbec4d765b767c2b62b3c160bc72cc26f3bd
Sean Oughton <seanoughton@gmail.com>
2018-07-12 19:02:05 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,12 +3,16 @@ class RepositoriesController < ApplicationController
     #response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     #@repos_array = JSON.parse(response.body)
     github = GithubService.new
+    github.initialize
     @repos_array = github.get_repos #returns an array of ruby objects
 
   end
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    #response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    github = GithubService.new
+    github.initialize
+    github.create_repo
     redirect_to '/'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     #response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
     #access_hash = JSON.parse(response.body)
     github = GithubService.new
+    github.initialize
     #session[:token] = access_hash["access_token"]
     session[:token] = github.authenticate!(ENV['GITHUB_CLIENT'], ENV['GITHUB_SECRET'], params[:code])
 
